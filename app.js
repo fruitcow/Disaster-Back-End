cd4903c59a965385153015424f9cd213d4965a83
Eric Wu <smart032410@gmail.com>
2015-12-02 11:05:07 +0800

    conv to geojson

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ async.series({
 
 },function(err,results) {
 	if(err)throw err;
-	result = GeoJSON.parse(result, {Point: ['Latitude', 'Longitude'], 'type':'Disaster', 'Magnitude':'Magnitude'});
+	result = GeoJSON.parse(result, {Point: ['Latitude', 'Longitude'], 'Disaster':'type', 'Magnitude':'Magnitude'});
 	console.log(result.length);
     fs.writeFileSync("./result/result.json",JSON.stringify(result));
     
