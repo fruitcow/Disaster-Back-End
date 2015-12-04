55a4d916124e3c2f0d8fa8ed8f16df74e37ba755
Eric Wu <smart032410@gmail.com>
2015-12-04 10:53:50 +0800

    fix Magnitude

--- modules/EarthQuakeParse.js
+++ modules/EarthQuakeParse.js
@@ -7,7 +7,22 @@ var  Earthquake =function(latitude,longitude,Earthquake){
 	this.type ="Earthquake";
 	this.Latitude=latitude;
 	this.Longitude=longitude;
-	this.Magnitude = Earthquake;
+
+	if(Earthquake > 7){
+		this.Magnitude = Extra Large;
+	}else if(Earthquake > 5){
+		this.Magnitude = Very_large;
+	}else if(Earthquake > 4){
+		this.Magnitude = Large;
+	}else if(Earthquake > 3){
+		this.Magnitude = Medium;
+	}else if(Earthquake > 2){
+		this.Magnitude = Small;
+	}else{
+		this.Magnitude = never;
+	}
+
+	
 }
 
 var DataSort = function(callback){
